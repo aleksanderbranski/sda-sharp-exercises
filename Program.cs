b1a284807a21d5418fcc45522a59472252f7f9f1
Aleksander Branski <andbranski@gmail.com>
2021-11-07 10:06:29 +0100

    E02 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace sda_sharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Podaj podstawę");
+            int basis = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Podaj wykładnik");
+            int exp = Convert.ToInt32(Console.ReadLine());
+            int result = 1;
+
+            if (exp>0)
+            {
+                for (int i = 0; i < exp; i++)
+                {
+                     result *= basis;
+                }
+
+                Console.WriteLine(result);
+            }
+            else if (exp<0)
+            {
+                for (int i = 0; i < exp; i++)
+                {
+                    result *= (1/basis);
+                }
+
+                Console.WriteLine(result);
+            }
+            else
+                Console.WriteLine("1");
         }
     }
 }
