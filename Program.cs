4a1429c700d8fc1a042a09811a69bb11eca9ffe0
Aleksander Branski <andbranski@gmail.com>
2021-11-07 09:56:42 +0100

    Zadanie E01 wykonane

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace sda_sharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Wpisz cenę");
+            int pc = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Wpisz ilość");
+            int qty = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= qty; i++)
+            {
+                Console.WriteLine($"Liczba sztuk: {i}; łączna cena: {pc * i}");
+            }
+           
         }
     }
 }
