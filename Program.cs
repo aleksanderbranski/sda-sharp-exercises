f580e772d6b717291a5e6e111d9bc4176ad5f572
Aleksander Branski <andbranski@gmail.com>
2021-11-07 09:46:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sda_sharp_exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
