1da366a9cb1e6acae61eab21b86732707775f5b5
Aleksander Branski <andbranski@gmail.com>
2021-11-07 12:31:00 +0100

    E05 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace sda_sharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Factorial(5));
+        }
+
+        static int Factorial(int number)
+        {
+            if (number <0)
+            {
+                throw new ArgumentException("Liczba musi być większa niż 0");
+            }
+            
+            int result = 1;
+
+            for (int i = number; i >1; i--)
+            {
+                result *= i;
+            }
+            return result;
         }
     }
 }
