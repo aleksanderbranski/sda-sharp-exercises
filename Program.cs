18ecf9e67a2fea3589f7d5345549a864d3e6f2c1
Aleksander Branski <andbranski@gmail.com>
2021-11-07 13:29:42 +0100

    E07 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace sda_sharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ToLeet("Leeat");
+            DeLeet("4$14 ");
+        }
+
+        static void ToLeet (string text)
+        {
+             char[] leetLetters = { '4', '3', '1', '0', '$' };
+             char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+
+          
+            string newtext = text;
+
+            for (int i = 0; i < leetLetters.Length; i++)
+            {
+                newtext = newtext.Replace(naturalLetters[i], leetLetters[i]);
+            }
+            
+          
+          
+            Console.WriteLine(newtext);
+        }
+
+        static void DeLeet(string text)
+        {
+            char[] leetLetters = { '4', '3', '1', '0', '$' };
+            char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+
+
+            string newtext = text;
+
+            for (int i = 0; i < leetLetters.Length; i++)
+            {
+                newtext = newtext.Replace(leetLetters[i], naturalLetters[i]);
+            }
+
+
+
+            Console.WriteLine(newtext);
         }
     }
 }
