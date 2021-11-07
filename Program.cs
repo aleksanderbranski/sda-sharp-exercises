9aa6d529ac1e8fefec81bbcb2879ab9a81104548
Aleksander Branski <andbranski@gmail.com>
2021-11-07 11:38:53 +0100

    E04 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace sda_sharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DrawTrg("*", 4);
+        }
+
+        static void DrawTrg(string symbol, int lineqty)
+        {
+            for (int i = 0; i < lineqty; i++)
+            {
+                for (int j = 0; j <= i ; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = lineqty; i > 0; i--)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write(symbol);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
