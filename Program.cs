ae841b6916ca1ec8a57683c075d635162f2393f7
Aleksander Branski <andbranski@gmail.com>
2021-11-07 11:00:07 +0100

    E03 done

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace sda_sharp_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(AProg(3, 1, 2)[i]);
+            }
         }
+
+        static int[] AProg(int length, int start, int diff)
+        {
+            int []result = new int [length];
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i==0)
+                {
+                    result[i] = start;
+                }
+                else
+                {
+                    result[i] = result[i-1]+ diff;
+                    
+                }
+                
+            }
+            return result;
+        }
+
     }
 }
